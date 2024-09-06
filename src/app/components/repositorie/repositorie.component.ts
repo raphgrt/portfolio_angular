cf9301e0e6ff08764c65da35a6e51dcfda6ac8ee
Raphael GEORGET <raphael.georgetpro@orange.fr>
2024-09-06 16:56:21 +0200

    [ERROR] repo animation

--- src/app/components/repositorie/repositorie.component.ts
+++ src/app/components/repositorie/repositorie.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @Component({
   selector: 'app-repositorie',
   standalone: true,
-  imports: [],
+  imports: [PanelModule, BrowserAnimationsModule],
   templateUrl: './repositorie.component.html',
   styleUrl: './repositorie.component.scss'
 })
-export class RepositorieComponent {
-
-}
+export class RepositorieComponent {}
